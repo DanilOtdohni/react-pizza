186ab776439ec36e6fb258166d8e2d70ecc6be8b
Danil Bychkov <daniloucheba102@gmail.com>
2022-05-12 22:18:34 +0500

    I made a popup using useEffect and useRef

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 
-function Categories({items, onClick}) {
+function Categories({items, onClickItem}) {
 
 	const state = React.useState(null);
-
 	const activeItem = state[0];
 	const setActiveItem = state[1];
-	console.log(activeItem);
+
+	const onSelectItem = (index) =>{
+		setActiveItem(index);
+	}
+
+
   return (
         <div className="categories">
 			<ul>
-				{
+			<li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>Все</li>
+				{items &&
 					items.map((name, index) => 
 					<li 
 						className={activeItem === index ? 'active' : ''} 
-						onClick={() => setActiveItem(index)} 
+						onClick={() => onSelectItem(index)} 
 						key={`${name}_${index}`}
 					>
 						{name}
